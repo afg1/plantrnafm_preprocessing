321f36598b3f91faced3e2577d4c3c78d0cbe5ac
Andrew Green <agreen@ebi.ac.uk>
2025-09-30 13:40:16 +0100

    Create a slurm submission script

---
+++ submit.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+#SBATCH --job-name=plantrnafm_preprocessing
+#SBATCH --output=nextflow_%j.log
+#SBATCH --error=nextflow_%j.err
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=4G
+
+# Load required modules
+module load nextflow
+
+# Run the Nextflow pipeline
+nextflow run main.nf -profile slurm

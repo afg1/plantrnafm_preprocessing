dea5c826704164b544bba84d8d6f2035f8c52908
Andrew Green <agreen@ebi.ac.uk>
2025-09-30 15:45:51 +0100

    Update job script output names

--- submit.sh
+++ submit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=plantrnafm_preprocessing
-#SBATCH --output=nextflow_%j.log
-#SBATCH --error=nextflow_%j.err
+#SBATCH --output=out_plrnafm
+#SBATCH --error=err_plrnafm
 #SBATCH --time=10:00:00
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=4G
